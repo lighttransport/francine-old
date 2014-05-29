81557859873d59d24c0e4e6ea71ed4a83c1bf38b
Syoyo Fujita <syoyo@lighttransport.com>
2014-05-29 23:43:24 +0900

    Read host config from a file.

--- render_teapot.sh
+++ render_teapot.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-curl http://localhost/sessions -XPOST -d \{\"InputJson\"\:\ \"scene/teapot_redis.json\"\}
-curl http://localhost/sessions/1/resources/scene/teapot_redis.json -XPUT --data-binary @demo/scene/teapot_redis.json
-curl http://localhost/sessions/1/resources/scene/teapot_scene.json -XPUT --data-binary @demo/scene/teapot_scene.json
-curl http://localhost/sessions/1/resources/scene/teapot.material.json -XPUT --data-binary @demo/scene/teapot.material.json
-curl http://localhost/sessions/1/resources/scene/shaders.json -XPUT --data-binary @demo/scene/shaders.json
-curl http://localhost/sessions/1/resources/scene/teapot.mesh -XPUT --data-binary @demo/scene/teapot.mesh
-curl http://localhost/sessions/1/resources/shader.c -XPUT --data-binary @demo/scene/shader.c
-curl http://localhost/sessions/1/resources/shader.h -XPUT --data-binary @demo/scene/shader.h
-curl http://localhost/sessions/1/resources/procedural-noise.c -XPUT --data-binary @demo/scene/procedural-noise.c
-curl http://localhost/sessions/1/resources/light.h -XPUT --data-binary @demo/scene/light.h
-curl -O http://localhost/sessions/1/renders -XPOST
+
+#set -x
+
+if [ ! -f hostfile ]; then
+  echo "hostfile not found"
+  exit 1
+fi
+
+MYHOST=`cat hostfile`
+SESSIONID=1
+curl http://${MYHOST}/sessions -XPOST -d \{\"InputJson\"\:\ \"scene/teapot_redis.json\"\}
+curl http://${MYHOST}/sessions/${SESSIONID}/resources/scene/teapot_redis.json -XPUT --data-binary @demo/scene/teapot_redis.json
+curl http://${MYHOST}/sessions/${SESSIONID}/resources/scene/teapot_scene.json -XPUT --data-binary @demo/scene/teapot_scene.json
+curl http://${MYHOST}/sessions/${SESSIONID}/resources/scene/teapot.material.json -XPUT --data-binary @demo/scene/teapot.material.json
+curl http://${MYHOST}/sessions/${SESSIONID}/resources/scene/shaders.json -XPUT --data-binary @demo/scene/shaders.json
+curl http://${MYHOST}/sessions/${SESSIONID}/resources/scene/teapot.mesh -XPUT --data-binary @demo/scene/teapot.mesh
+curl http://${MYHOST}/sessions/${SESSIONID}/resources/shader.c -XPUT --data-binary @demo/scene/shader.c
+curl http://${MYHOST}/sessions/${SESSIONID}/resources/shader.h -XPUT --data-binary @demo/scene/shader.h
+curl http://${MYHOST}/sessions/${SESSIONID}/resources/procedural-noise.c -XPUT --data-binary @demo/scene/procedural-noise.c
+curl http://${MYHOST}/sessions/${SESSIONID}/resources/light.h -XPUT --data-binary @demo/scene/light.h
+curl -O http://${MYHOST}/sessions/${SESSIONID}/renders -XPOST
 
