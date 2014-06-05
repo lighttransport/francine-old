ddd28119d5737409eb847516993621c3fe74ab42
Syoyo Fujita <syoyo@lighttransport.com>
2014-06-05 22:28:01 +0900

    Don't assign external IP for worker instance.

--- master/master.go
+++ master/master.go
@@ -122,10 +122,11 @@ func createWorkerInstance(etcdHost string) {
 			"zone":       "https://www.googleapis.com/compute/v1/projects/gcp-samples/zones/" + zone,
 			"source":     "https://www.googleapis.com/compute/v1/projects/gcp-samples/zones/" + zone + "/disks/" + instanceName}},
 		"networkInterfaces": []interface{}{map[string]interface{}{
-			"network": "https://www.googleapis.com/compute/v1/projects/gcp-samples/global/networks/lte-cluster",
-			"accessConfigs": []interface{}{map[string]string{
-				"name": "External NAT",
-				"type": "ONE_TO_ONE_NAT"}}}},
+			"network": "https://www.googleapis.com/compute/v1/projects/gcp-samples/global/networks/lte-cluster"}},
+			// NOTE: no accessConfigs[] = will have no external internet access
+			//"accessConfigs": []interface{}{map[string]string{
+			//	"name": "External NAT",
+			//	"type": "ONE_TO_ONE_NAT"}}}},
 		"metadata": map[string]interface{}{
 			"items": []interface{}{
 				map[string]string{
